c886df561d638e9b3bfc36c0a1153b1442f77696
Merli <merlimuuk@gmail.com>
2021-04-19 21:07:25 +0300

    Update

--- Assets/Scripts/DestroyOnHit.cs
+++ Assets/Scripts/DestroyOnHit.cs
@@ -3,8 +3,7 @@ using System.Collections;
 
 public class DestroyOnHit : MonoBehaviour
 {
-
-    void OnTriggerEnter2D(Collider2D other)
+	void OnTriggerEnter2D(Collider2D other)
     {
         Destroy(other.gameObject);
     }
